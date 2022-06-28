ddbf43825b66b2fcf41f5443bc51a1e5ccac27fc
Philemon Philippin <p_philippin@hotmail.com>
2022-06-28 22:54:59 +0200

    Post form for member register done dont know if functional

---
+++ Bugtracker.WASM/Pages/MemberComponents/RegisterMember.cs
@@ -0,0 +1,30 @@
+﻿using Bugtracker.WASM.ViewModels;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace Bugtracker.WASM.Pages.MemberComponents
+{
+    public partial class RegisterMember
+    {
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        private MemberRegistrationVm _member;
+
+        protected MemberRegistrationVm Member
+        {
+            get { return _member; }
+            set { _member = value; }
+        }
+        public RegisterMember()
+        {
+            _member = new MemberRegistrationVm();
+        }
+        private async Task SubmitRegistration()
+        {
+            // TODO : mettre à jour l'uri du post
+            using var response = await Http.PutAsJsonAsync("ChangeMyURIHere", _member);
+            MemberRegistrationVm responseMember = await response.Content.ReadFromJsonAsync<MemberRegistrationVm>();
+        }
+    }
+}
--- Bugtracker.WASM/ViewModels/MemberVm.cs
+++ Bugtracker.WASM/ViewModels/MemberVm.cs
@@ -1,4 +1,6 @@
-﻿namespace Bugtracker.WASM.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bugtracker.WASM.ViewModels
 {
     public class MemberVm
     {
@@ -8,5 +10,20 @@
         public string EmailAddress { get; set; }
         public string? Firstname { get; set; }
         public string? Lastname { get; set; }
+    }
+    public class MemberRegistrationVm
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
     }
 }
