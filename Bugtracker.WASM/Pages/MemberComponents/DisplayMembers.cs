e77916d70a1a820cad650b9f2beaad6a3fd59ec1
Tarcali0n <p_philippin@hotmail.com>
2022-06-28 14:57:55 +0200

    commit not functional

--- Bugtracker.WASM/Pages/MemberComponents/DisplayMembers.cs
+++ Bugtracker.WASM/Pages/MemberComponents/DisplayMembers.cs
@@ -1,7 +1,19 @@
-﻿namespace Bugtracker.WASM.Pages.MemberComponents
+﻿using Bugtracker.WASM.ViewModels;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace Bugtracker.WASM.Pages.MemberComponents
 {
     public partial class DisplayMembers
     {
+        [Inject]
+        public HttpClient Http { get; set; }
+        public List<MemberVm> members { get; set; } = new List<MemberVm>();
 
+        protected override async Task OnInitializedAsync()
+        {
+            members = await Http.GetFromJsonAsync<List<MemberVm>>("https://localhost:7051/api/Member");
+            Console.WriteLine("On est entré dans la consommation d'API");
+        }
     }
 }
