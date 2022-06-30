1c2ea0741d69d10a0f78d815f00fdf5ebe4e45ed
Philemon Philippin <p_philippin@hotmail.com>
2022-06-30 12:47:22 +0200

    added bools to check if login or email or both already taken but console write line dont work in blazor

--- Bugtracker.WASM/Pages/MemberComponents/RegisterMember.cs
+++ Bugtracker.WASM/Pages/MemberComponents/RegisterMember.cs
@@ -8,7 +8,8 @@ namespace Bugtracker.WASM.Pages.MemberComponents
     {
         [Inject]
         public HttpClient Http { get; set; }
-
+        public bool isLoginTaken = false;
+        public bool isEmailTaken = false;
         public MemberRegistrationVm member;
         public RegisterMember()
         {
@@ -19,6 +20,41 @@ namespace Bugtracker.WASM.Pages.MemberComponents
             HttpResponseMessage response = await Http.PostAsJsonAsync("https://localhost:7051/api/Member", member);
             // if I ever need to get back the member that I posted :
             // MemberRegistrationVm responseMember = await response.Content.ReadFromJsonAsync<MemberRegistrationVm>();
+            var responseObject = await response.Content.ReadFromJsonAsync<object?>();
+            if (responseObject is not null)
+            {
+                switch (responseObject)
+                {
+                    case int i:
+                        switch (i)
+                        {
+                            case -123:
+                                isLoginTaken = true;
+                                // TODO : deal with the state of isLoginTaken to show to the user that login is taken
+                                Console.WriteLine("Login taken");
+                                break;
+                            case -456:
+                                Console.WriteLine("Email taken");
+                                isEmailTaken = true;
+                                break;
+                            case -789:
+                                isLoginTaken = true;
+                                isEmailTaken = true;
+                                Console.WriteLine("Both Login and Email taken");
+                                break;
+                            default:
+                                Console.WriteLine("Response object not as expected");
+                                break;
+                        }
+                        break;
+                    case MemberRegistrationVm responseMember:
+                        // TODO : do i need to do something with the member i get back ?
+                        break;
+                    default:
+                        Console.WriteLine("Response object not as expected");
+                        break;
+                }
+            }
             member = new MemberRegistrationVm();
         }
     }
