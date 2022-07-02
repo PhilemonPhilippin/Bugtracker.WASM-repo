76dfb3d934de45ac0f9125989891ec684de5a834
Philemon Philippin <p_philippin@hotmail.com>
2022-07-02 12:05:56 +0200

    edit member done

--- Bugtracker.WASM/Pages/MemberComponents/EditMember.cs
+++ Bugtracker.WASM/Pages/MemberComponents/EditMember.cs
@@ -11,34 +11,44 @@ namespace Bugtracker.WASM.Pages.MemberComponents
         // Faut-il initialiser le MemberTarget?
         // Ou faut-il faire une fonction async on initialize pour le MemberEditModel ?
         [Parameter]
-        public MemberModel MemberTarget { get; set; }
-
-        public MemberEditModel memberEdit;
+        public int TargetId { get; set; }
+        public MemberEditModel MemberEditModel { get; set; } = new MemberEditModel();
         public bool isPseudoTaken = false;
         public bool isEmailTaken = false;
-        public EditMember()
+
+        protected override async Task OnInitializedAsync()
         {
-            MemberTarget = new MemberModel();
-            memberEdit = new MemberEditModel()
+            //MemberModel memberModel = await Http.GetFromJsonAsync<MemberModel>($"https://localhost:7051/api/Member/{TargetId}");
+            HttpResponseMessage response = await Http.GetAsync($"https://localhost:7051/api/Member/{TargetId}");
+            if (response.IsSuccessStatusCode)
             {
-                IdMember = MemberTarget.IdMember,
-                Pseudo = MemberTarget.Pseudo,
-                Password = MemberTarget.PswdHash,
-                Email = MemberTarget.Email,
-                Firstname = MemberTarget.Firstname,
-                Lastname = MemberTarget.Lastname
-            };
+                MemberModel memberModel = await response.Content.ReadFromJsonAsync<MemberModel>();
+                MemberEditModel.IdMember = memberModel.IdMember;
+                MemberEditModel.Pseudo = memberModel.Pseudo;
+                MemberEditModel.Email = memberModel.Email;
+                MemberEditModel.Password = memberModel.PswdHash;
+                MemberEditModel.Firstname = memberModel.Firstname;
+                MemberEditModel.Lastname = memberModel.Lastname;
+            }
         }
-        private async Task SubmitEdit(int id)
+        private async Task SubmitEdit()
         {
             isPseudoTaken = false;
             isEmailTaken = false;
-            // TODO : finir cette fonction et le formulaire qui va avec
-            HttpResponseMessage response = await Http.PutAsJsonAsync($"https://localhost:7051/api/Member/{id}", memberEdit);
+            MemberModel memberModel = new MemberModel()
+            {
+                IdMember = MemberEditModel.IdMember,
+                Pseudo = MemberEditModel.Pseudo,
+                Email = MemberEditModel.Email,
+                PswdHash = MemberEditModel.Password,
+                Firstname = MemberEditModel.Firstname,
+                Lastname = MemberEditModel.Lastname
+            };
+            HttpResponseMessage response = await Http.PutAsJsonAsync($"https://localhost:7051/api/Member/{TargetId}", memberModel);
 
             if (!response.IsSuccessStatusCode)
             {
-                int responseNumber = await response.Content.ReadFromJsonAsync<int>();
+               
             }
             else
                 Console.WriteLine("Close the Edit member component");
