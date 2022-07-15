250c3c7a5323ffbbbc449de2b61762183547c099
Philemon Philippin <p_philippin@hotmail.com>
2022-07-15 08:11:56 +0200

    List of projects displayed

---
+++ Bugtracker.WASM/Models/ProjectModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bugtracker.WASM.Models
+{
+    public class ProjectModel
+    {
+        public int IdProject { get; set; }
+        [MaxLength(250)]
+        public string Name { get; set; }
+        [MaxLength(750)]
+        public string Description { get; set; }
+        public int Manager { get; set; }
+    }
+}
--- Bugtracker.WASM/Pages/ProjectComponents/DisplayProjects.cs
+++ Bugtracker.WASM/Pages/ProjectComponents/DisplayProjects.cs
@@ -1,6 +1,35 @@
-﻿namespace Bugtracker.WASM.Pages.ProjectComponents
+﻿using Bugtracker.WASM.Tools;
+using Microsoft.AspNetCore.Components;
+using static System.Net.WebRequestMethods;
+using System.Net.Http.Headers;
+using Bugtracker.WASM.Models;
+using System.Net.Http.Json;
+
+namespace Bugtracker.WASM.Pages.ProjectComponents
 {
     public partial class DisplayProjects
     {
+        [Inject]
+        public HttpClient Http { get; set; }
+        [Inject]
+        IMemberLocalStorage LocalStorage { get; set; }
+        private List<ProjectModel> _projects = new List<ProjectModel>();
+        private bool _isMemberConnected;
+        private string _token;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _token = await LocalStorage.GetToken();
+            if (_token is null)
+                _isMemberConnected = false;
+            else
+                _isMemberConnected = true;
+
+            if (_isMemberConnected)
+            {
+                Http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+                _projects = await Http.GetFromJsonAsync<List<ProjectModel>>("https://localhost:7051/api/Project");
+            }
+        }
     }
 }
