b73be7e3a4bfa1a2d32093995833373cfde6a135
Philemon Philippin <p_philippin@hotmail.com>
2022-07-15 08:37:15 +0200

    Created display projects and details projects

---
+++ Bugtracker.WASM/Pages/ProjectComponents/DetailsProject.cs
@@ -0,0 +1,13 @@
+﻿using Bugtracker.WASM.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace Bugtracker.WASM.Pages.ProjectComponents
+{
+    public partial class DetailsProject
+    {
+        [Parameter]
+        public ProjectModel ProjectTarget { get; set; }
+        [Parameter]
+        public EventCallback OnCancel { get; set; }
+    }
+}
--- Bugtracker.WASM/Pages/ProjectComponents/DisplayProjects.cs
+++ Bugtracker.WASM/Pages/ProjectComponents/DisplayProjects.cs
@@ -4,6 +4,7 @@ using static System.Net.WebRequestMethods;
 using System.Net.Http.Headers;
 using Bugtracker.WASM.Models;
 using System.Net.Http.Json;
+using System.Diagnostics.Metrics;
 
 namespace Bugtracker.WASM.Pages.ProjectComponents
 {
@@ -14,6 +15,9 @@ namespace Bugtracker.WASM.Pages.ProjectComponents
         [Inject]
         IMemberLocalStorage LocalStorage { get; set; }
         private List<ProjectModel> _projects = new List<ProjectModel>();
+        private ProjectModel _projectTarget;
+        private bool _displayProjectDetailsDialog;
+        private bool _displayProjectEditDialog;
         private bool _isMemberConnected;
         private string _token;
 
@@ -30,6 +34,51 @@ namespace Bugtracker.WASM.Pages.ProjectComponents
                 Http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
                 _projects = await Http.GetFromJsonAsync<List<ProjectModel>>("https://localhost:7051/api/Project");
             }
+        }
+        private async Task DeleteProject(int id)
+        {
+            await Http.DeleteAsync($"https://localhost:7051/api/Project/{id}");
+            await RefreshProjectsList();
+        }
+        private async Task RefreshProjectsList()
+        {
+            _token = await LocalStorage.GetToken();
+            if (_token is null)
+                _isMemberConnected = false;
+            else
+                _isMemberConnected = true;
+
+            if (_isMemberConnected)
+            {
+                Http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+                _projects = await Http.GetFromJsonAsync<List<ProjectModel>>("https://localhost:7051/api/Project");
+            }
+        }
+        private void DisplayProjectDetailsDialog(ProjectModel project)
+        {
+            if (_displayProjectDetailsDialog)
+                _displayProjectDetailsDialog = false;
+            else
+            {
+                _displayProjectEditDialog = false;
+                _displayProjectDetailsDialog = true;
+                _projectTarget = project;
+            }
+        }
+        private void DisplayProjectEditDialog(ProjectModel project)
+        {
+            if (_displayProjectEditDialog)
+                _displayProjectEditDialog = false;
+            else
+            {
+                _displayProjectDetailsDialog = false;
+                _displayProjectEditDialog = true;
+                _projectTarget = project;
+            }
+        }
+        private void CloseDetailsDialog()
+        {
+            _displayProjectDetailsDialog = false;
         }
     }
 }
