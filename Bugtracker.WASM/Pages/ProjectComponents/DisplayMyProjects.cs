a714da01a7a3f520525458f8cce61e42c9d7ed28
Philemon Philippin <p_philippin@hotmail.com>
2022-07-23 16:25:40 +0200

    edit in display my projects totally done

--- Bugtracker.WASM/Pages/ProjectComponents/DisplayMyProjects.cs
+++ Bugtracker.WASM/Pages/ProjectComponents/DisplayMyProjects.cs
@@ -23,6 +23,7 @@ namespace Bugtracker.WASM.Pages.ProjectComponents
         private string _token;
         private bool _isMemberConnected;
         private bool _displayProjectDetailsDialog;
+        private bool _displayEditProjectDialog;
 
         protected override async Task OnInitializedAsync()
         {
@@ -35,8 +36,8 @@ namespace Bugtracker.WASM.Pages.ProjectComponents
                 _members = await Http.GetFromJsonAsync<List<MemberModel>>("https://localhost:7051/api/Member");
                 _myMemberId = await Http.GetFromJsonAsync<int?>("https://localhost:7051/api/Member/idfromjwt");
                 _assigns = await Http.GetFromJsonAsync<List<AssignModel>>("https://localhost:7051/api/Assign");
-                List<int> myProjectsId = new List<int>();
                 // Je crée une liste avec les projects id pour lesquels le membre connecté est assigné.
+                List<int> myProjectsId = new List<int>();
                 foreach (AssignModel assign in _assigns)
                 {
                     if (assign.Member == _myMemberId)
@@ -49,14 +50,36 @@ namespace Bugtracker.WASM.Pages.ProjectComponents
                     _myProjects = _projects.Where(prj => myProjectsId.Contains(prj.IdProject)).ToList();
             }
         }
-
+        private async Task RefreshProjectsList()
+        {
+            _isMemberConnected = await LocalStorage.HasToken();
+            if (_isMemberConnected)
+            {
+                _token = await LocalStorage.GetToken();
+                Http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+                _projects = await Http.GetFromJsonAsync<List<ProjectModel>>("https://localhost:7051/api/Project");
+                _assigns = await Http.GetFromJsonAsync<List<AssignModel>>("https://localhost:7051/api/Assign");
+                // Je crée une liste avec les projects id pour lesquels le membre connecté est assigné.
+                List<int> myProjectsId = new List<int>();
+                foreach (AssignModel assign in _assigns)
+                {
+                    if (assign.Member == _myMemberId)
+                    {
+                        myProjectsId.Add(assign.Project);
+                    }
+                }
+                // Je fais une liste avec mes projets pour lesquels l'id se trouve dans la liste que j'ai créée plus haut.
+                if (myProjectsId.Count > 0)
+                    _myProjects = _projects.Where(prj => myProjectsId.Contains(prj.IdProject)).ToList();
+            }
+        }
         private void DisplayProjectDetailsDialog(ProjectModel project)
         {
             if (_displayProjectDetailsDialog)
                 _displayProjectDetailsDialog = false;
             else
             {
-                //_displayEditProjectDialog = false;
+                _displayEditProjectDialog = false;
                 _displayProjectDetailsDialog = true;
                 _projectTarget = project;
             }
@@ -64,6 +87,26 @@ namespace Bugtracker.WASM.Pages.ProjectComponents
         private void CloseDetailsDialog()
         {
             _displayProjectDetailsDialog = false;
+        }
+        private void DisplayProjectEditDialog(ProjectModel project)
+        {
+            if (_displayEditProjectDialog)
+                _displayEditProjectDialog = false;
+            else
+            {
+                _displayProjectDetailsDialog = false;
+                _displayEditProjectDialog = true;
+                _projectTarget = project;
+            }
+        }
+        private void CloseEditDialog()
+        {
+            _displayEditProjectDialog = false;
+        }
+        private async Task ConfirmProjectEdit()
+        {
+            await RefreshProjectsList();
+            _displayEditProjectDialog = false;
         }
     }
 }
