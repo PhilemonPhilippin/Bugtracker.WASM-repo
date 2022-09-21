2bbd5ba367398e52dd6d6764a4298dca6a0731c6
Philemon Philippin <p_philippin@hotmail.com>
2022-09-21 11:09:05 +0200

    ProjectEdit done

---
+++ Bugtracker.WASM/Shared/ProjectComponents/ProjectEdit.razor.cs
@@ -0,0 +1,61 @@
+﻿using Bugtracker.WASM.Models.MemberModels;
+using Bugtracker.WASM.Models;
+using Bugtracker.WASM.Tools;
+using Microsoft.AspNetCore.Components;
+using Bugtracker.WASM.Mappers;
+
+namespace Bugtracker.WASM.Shared.ProjectComponents
+{
+    public partial class ProjectEdit
+    {
+        [Inject]
+        private IMemberLocalStorage LocalStorage { get; set; } = default!;
+        [Inject]
+        private IApiRequester Requester { get; set; } = default!;
+        [Parameter]
+        public EventCallback OnCancel { get; set; }
+        [Parameter]
+        public EventCallback OnConfirm { get; set; }
+        [Parameter]
+        public ProjectModel ProjectTarget { get; set; }
+        private ProjectFormModel EditedProject { get; set; } = new ProjectFormModel();
+        private List<MemberModel> _members = new List<MemberModel>();
+        private bool _isMemberConnected;
+        private bool _displayNameTaken;
+        private string _token;
+        protected override async Task OnInitializedAsync()
+        {
+            EditedProject.IdProject = ProjectTarget.IdProject;
+            EditedProject.Name = ProjectTarget.Name;
+            EditedProject.Description = ProjectTarget.Description;
+            EditedProject.Manager = ProjectTarget.Manager;
+
+            _token = await LocalStorage.GetToken();
+            if (_token is not null)
+            {
+                _isMemberConnected = true;
+                _members = await Requester.Get<List<MemberModel>>("Member", _token);
+            }
+        }
+        private async Task SubmitEdit()
+        {
+            _token = await LocalStorage.GetToken();
+            if (_token is not null)
+            {
+                _isMemberConnected = true;
+                _displayNameTaken = false;
+                ProjectModel projectModel = EditedProject.ToModel();
+
+                using HttpResponseMessage response = await Requester.Put(projectModel, "Project", _token);
+                if (!response.IsSuccessStatusCode)
+                {
+                    string errorMessage = await response.Content.ReadAsStringAsync();
+                    if (errorMessage.Contains("Name already exists."))
+                        _displayNameTaken = true;
+                }
+                else
+                    await OnConfirm.InvokeAsync();
+            }
+        }
+    }
+}
