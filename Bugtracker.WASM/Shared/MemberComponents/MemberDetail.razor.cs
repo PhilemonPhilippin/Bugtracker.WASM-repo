c5f72c06a28f4c67d4ec8e8e4994fa99983d599a
Philemon Philippin <p_philippin@hotmail.com>
2022-09-21 11:28:49 +0200

    MemberDetail done

---
+++ Bugtracker.WASM/Shared/MemberComponents/MemberDetail.razor.cs
@@ -0,0 +1,13 @@
+﻿using Bugtracker.WASM.Models.MemberModels;
+using Microsoft.AspNetCore.Components;
+
+namespace Bugtracker.WASM.Shared.MemberComponents
+{
+    public partial class MemberDetail
+    {
+        [Parameter]
+        public MemberModel MemberTarget { get; set; }
+        [Parameter]
+        public EventCallback OnCancel { get; set; }
+    }
+}
