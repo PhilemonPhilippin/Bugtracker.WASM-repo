ba1a69a9b843274b89d173e0bd4e3e00dfe4a8ab
Philemon Philippin <p_philippin@hotmail.com>
2022-09-21 11:38:27 +0200

    Logout done

---
+++ Bugtracker.WASM/Pages/Logout.razor.cs
@@ -0,0 +1,31 @@
+﻿using Bugtracker.WASM.Tools;
+using Microsoft.AspNetCore.Components;
+
+namespace Bugtracker.WASM.Pages
+{
+    public partial class Logout
+    {
+        [Inject]
+        private IMemberLocalStorage LocalStorage { get; set; } = default!;
+        [Inject]
+        private NavigationManager NavManager { get; set; } = default!;
+        private bool _isMemberConnected;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _isMemberConnected = await LocalStorage.HasToken();
+            if (!_isMemberConnected)
+                NavManager.NavigateTo("/account");
+        }
+        private async Task ConfirmLogout()
+        {
+            await LocalStorage.RemoveToken();
+            _isMemberConnected = false;
+            NavManager.NavigateTo("/account");
+        }
+        private void ToDashboard()
+        {
+            NavManager.NavigateTo("/");
+        }
+    }
+}
---
+++ Bugtracker.WASM/Shared/MemberComponents/MemberChangePswd.razor.cs
@@ -0,0 +1,53 @@
+﻿using Bugtracker.WASM.Models.MemberModels;
+using Bugtracker.WASM.Tools;
+using Microsoft.AspNetCore.Components;
+
+namespace Bugtracker.WASM.Shared.MemberComponents
+{
+    public partial class MemberChangePswd
+    {
+        [Inject]
+        private IMemberLocalStorage LocalStorage { get; set; } = default!;
+        [Inject]
+        private IApiRequester Requester { get; set; } = default!;
+        [Parameter]
+        public int MemberId { get; set; }
+        [Parameter]
+        public EventCallback OnCancel { get; set; }
+        private MemberFormPswdModel pswdModel = new MemberFormPswdModel();
+        private MemberPostPswdModel postPswdModel = new MemberPostPswdModel();
+        private bool _displayIncorrectPassword;
+        private bool _displayPasswordChanged;
+        private bool _isMemberConnected;
+        private string _token;
+
+        private async Task Submit()
+        {
+            _displayIncorrectPassword = false;
+            _displayPasswordChanged = false;
+
+            _token = await LocalStorage.GetToken();
+            if (_token is not null)
+            {
+                _isMemberConnected = true;
+                postPswdModel.IdMember = MemberId;
+                postPswdModel.NewPassword = pswdModel.NewPassword;
+                postPswdModel.OldPassword = pswdModel.OldPassword;
+
+                using HttpResponseMessage response = await Requester.Post(postPswdModel, "Member/changepswd", _token);
+                if (!response.IsSuccessStatusCode)
+                {
+                    string errorMessage = await response.Content.ReadAsStringAsync();
+
+                    if (errorMessage.Contains("Password incorrect."))
+                        _displayIncorrectPassword = true;
+                }
+                else
+                {
+                    _displayPasswordChanged = true;
+                    pswdModel = new MemberFormPswdModel();
+                }
+            }
+        }
+    }
+}
