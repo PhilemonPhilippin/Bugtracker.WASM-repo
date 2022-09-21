36c175eaf4f33058b2e76e86975472f85770795b
Philemon Philippin <p_philippin@hotmail.com>
2022-09-21 11:31:53 +0200

    MemberEdit done

---
+++ Bugtracker.WASM/Shared/MemberComponents/MemberEdit.razor.cs
@@ -0,0 +1,66 @@
+﻿using Bugtracker.WASM.Models.MemberModels;
+using Bugtracker.WASM.Tools;
+using Microsoft.AspNetCore.Components;
+
+namespace Bugtracker.WASM.Shared.MemberComponents
+{
+    public partial class MemberEdit
+    {
+        [Inject]
+        private IMemberLocalStorage LocalStorage { get; set; } = default!;
+        [Inject]
+        private IApiRequester Requester { get; set; } = default!;
+        [Parameter]
+        public MemberModel MemberTarget { get; set; }
+        [Parameter]
+        public EventCallback OnCancel { get; set; }
+        [Parameter]
+        public EventCallback OnConfirm { get; set; }
+        private string _token;
+        private MemberModel MemberEdited { get; set; } = new MemberModel();
+        private bool _displayPseudoTaken;
+        private bool _displayEmailTaken;
+        private bool _isMemberConnected;
+
+        protected override async Task OnInitializedAsync()
+        {
+            MemberEdited.IdMember = MemberTarget.IdMember;
+            MemberEdited.Pseudo = MemberTarget.Pseudo;
+            MemberEdited.Email = MemberTarget.Email;
+            MemberEdited.Firstname = MemberTarget.Firstname;
+            MemberEdited.Lastname = MemberTarget.Lastname;
+        }
+        private async Task SubmitEdit()
+        {
+            _displayPseudoTaken = false;
+            _displayEmailTaken = false;
+
+            _token = await LocalStorage.GetToken();
+            if (_token is not null)
+            {
+                _isMemberConnected = true;
+                using HttpResponseMessage response = await Requester.Put(MemberEdited, "Member", _token);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    string errorMessage = await response.Content.ReadAsStringAsync();
+                    HandleErrorMessage(errorMessage);
+                }
+                else
+                    await OnConfirm.InvokeAsync();
+            }
+        }
+        private void HandleErrorMessage(string errorMessage)
+        {
+            if (errorMessage.Contains("Pseudo and Email already exist."))
+            {
+                _displayPseudoTaken = true;
+                _displayEmailTaken = true;
+            }
+            else if (errorMessage.Contains("Pseudo already exists."))
+                _displayPseudoTaken = true;
+            else if (errorMessage.Contains("Email already exists."))
+                _displayEmailTaken = true;
+        }
+    }
+}
