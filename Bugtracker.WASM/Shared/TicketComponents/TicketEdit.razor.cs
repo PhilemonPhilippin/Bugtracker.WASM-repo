fd453acd0b99f957f0547571c35f287732aa8844
Philemon Philippin <p_philippin@hotmail.com>
2022-09-21 11:19:04 +0200

    TicketEdit done

---
+++ Bugtracker.WASM/Shared/TicketComponents/TicketEdit.razor.cs
@@ -0,0 +1,58 @@
+﻿using Bugtracker.WASM.Mappers;
+using Bugtracker.WASM.Models;
+using Bugtracker.WASM.Tools;
+using Microsoft.AspNetCore.Components;
+
+namespace Bugtracker.WASM.Shared.TicketComponents
+{
+    public partial class TicketEdit
+    {
+        [Inject]
+        private IMemberLocalStorage LocalStorage { get; set; } = default!;
+        [Inject]
+        private IApiRequester Requester { get; set; } = default!;
+        [Parameter]
+        public EventCallback OnCancel { get; set; }
+        [Parameter]
+        public EventCallback OnConfirm { get; set; }
+        [Parameter]
+        public TicketModel TicketTarget { get; set; }
+        private List<ProjectModel> _projects = new List<ProjectModel>();
+        private TicketEditModel EditedTicket { get; set; } = new TicketEditModel();
+        private string _token;
+        private bool _isMemberConnected;
+        private bool _displayTitleTaken;
+
+        protected override async Task OnInitializedAsync()
+        {
+            EditedTicket = TicketTarget.ToEditModel();
+
+            _token = await LocalStorage.GetToken();
+            if (_token is not null)
+            {
+                _isMemberConnected = true;
+                _projects = await Requester.Get<List<ProjectModel>>("Project", _token);
+            }
+        }
+        private async Task SubmitEdit()
+        {
+            _token = await LocalStorage.GetToken();
+            if (_token is not null)
+            {
+                _isMemberConnected = true;
+                _displayTitleTaken = false;
+                TicketModel ticketModel = EditedTicket.ToModel();
+
+                using HttpResponseMessage response = await Requester.Put(ticketModel, "Ticket", _token);
+                if (!response.IsSuccessStatusCode)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    if (message.Contains("Title already exists."))
+                        _displayTitleTaken = true;
+                }
+                else
+                    await OnConfirm.InvokeAsync();
+            }
+        }
+    }
+}
