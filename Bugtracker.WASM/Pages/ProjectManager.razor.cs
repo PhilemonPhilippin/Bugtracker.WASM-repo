4172c172419d050a0d265b55949cc89395c4a44d
Philemon Philippin <p_philippin@hotmail.com>
2022-09-26 14:15:23 +0200

    Access to project manager restricted to only higher roles

--- Bugtracker.WASM/Pages/ProjectManager.razor.cs
+++ Bugtracker.WASM/Pages/ProjectManager.razor.cs
@@ -1,4 +1,5 @@
-﻿using Bugtracker.WASM.Tools;
+﻿using Bugtracker.WASM.Models.MemberModels;
+using Bugtracker.WASM.Tools;
 using Microsoft.AspNetCore.Components;
 
 namespace Bugtracker.WASM.Pages
@@ -9,19 +10,37 @@ namespace Bugtracker.WASM.Pages
         private NavigationManager NavManager { get; set; } = default!;
         [Inject]
         private IMemberLocalStorage LocalStorage { get; set; } = default!;
+        [Inject]
+        private IApiRequester Requester { get; set; } = default!;
+        private MemberModel _myMemberModel = new();
         private bool _isMemberConnected;
         private bool _displayProjectsComponent = true;
         private bool _displayTicketsComponent;
+        private string _token;
+        private int _myMemberId;
 
         protected override async Task OnInitializedAsync()
         {
-            _isMemberConnected = await LocalStorage.HasToken();
-            if (!_isMemberConnected)
+            _token = await LocalStorage.GetToken();
+            if (_token is not null)
+            {
+                _isMemberConnected = true;
+                _myMemberId = await Requester.Get<int>("Member/idfromjwt", _token);
+                _myMemberModel = await Requester.Get<MemberModel>($"Member/{_myMemberId}", _token);
+            }
+            else
+            {
+                _isMemberConnected = false;
                 NavManager.NavigateTo("/account");
+            }
         }
         private void ToAccount()
         {
             NavManager.NavigateTo("/account");
+        }
+        private void ToDashboard()
+        {
+            NavManager.NavigateTo("");
         }
         private void DisplayProjectsComponent()
         {
