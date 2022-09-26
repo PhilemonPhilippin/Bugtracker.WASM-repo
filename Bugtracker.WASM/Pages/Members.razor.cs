cd490da4ca89cff7bc31768d271d7c48f3ad1f55
Philemon Philippin <p_philippin@hotmail.com>
2022-09-26 14:25:02 +0200

    access to members management restricted to administrators or higher

--- Bugtracker.WASM/Pages/Members.razor.cs
+++ Bugtracker.WASM/Pages/Members.razor.cs
@@ -1,4 +1,5 @@
-﻿using Bugtracker.WASM.Tools;
+﻿using Bugtracker.WASM.Models.MemberModels;
+using Bugtracker.WASM.Tools;
 using Microsoft.AspNetCore.Components;
 
 namespace Bugtracker.WASM.Pages
@@ -9,13 +10,28 @@ namespace Bugtracker.WASM.Pages
         private NavigationManager NavManager { get; set; } = default!;
         [Inject]
         private IMemberLocalStorage LocalStorage { get; set; } = default!;
+        [Inject]
+        private IApiRequester Requester { get; set; } = default!;
+
+        private MemberModel _myMemberModel = new();
         private bool _isMemberConnected;
+        private string _token;
+        private int _myMemberId;
 
         protected override async Task OnInitializedAsync()
         {
-            _isMemberConnected = await LocalStorage.HasToken();
-            if (!_isMemberConnected)
+            _token = await LocalStorage.GetToken();
+            if (_token is null)
+            {
+                _isMemberConnected = false;
                 NavManager.NavigateTo("/account");
+            }
+            else
+            {
+                _isMemberConnected = true;
+                _myMemberId = await Requester.Get<int>("Member/idfromjwt", _token);
+                _myMemberModel = await Requester.Get<MemberModel>($"Member/{_myMemberId}", _token);
+            }
         }
         private void ToAccount()
         {
